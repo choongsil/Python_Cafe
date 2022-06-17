16b5b991eeb69ad2700777578248e504e6a1d709
choongsil <s.c.sil35@gmail.com>
2022-06-17 14:23:09 +0900

    Cafe sales Class

--- Sales.py
+++ Sales.py
@@ -5,15 +5,16 @@ class Sales():
         self.mobile=[]
         self.total= []
         self.created=[]
-    def add(self, mobile, price):
+    def add(self, mobile, price, date):
         self.mobile.append(mobile)
         self.total.append(price)
+        self.created.append(date)
 
     def sum(self):
         #매출목록 보여주고
         print('매출목록')
         for i in range(len(self.mobile)):
-            print(self.mobile[i], self.total[i],'원')
+            print(self.mobile[i], self.total[i],'원',self.created[i])
         #매출총액계산/출력
         print('매출 총액')
         print(sum(self.total),'원')
