6b0d08a18ecf0a85160dbdf8b469ad06ecfa8846
choongsil <s.c.sil35@gmail.com>
2022-06-17 11:51:48 +0900

    cafe order Class

---
+++ Order.py
@@ -0,0 +1,14 @@
+class Order:
+    def __init__(self):
+        self.menu_no=[]
+        self.qty=[]
+        self.price=[]
+    def add(self, menu_no, qty, price):
+        self.menu_no.append(menu_no)
+        self.qty.append(qty)
+        self.price.append(price*qty)
+
+    def cancel(self):
+        self.menu_no.clear()
+        self.qty.clear()
+        self.price.clear()
