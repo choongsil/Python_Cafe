28683d7b35f72253b0ccd2c303d19976d8eda6b8
choongsil <s.c.sil35@gmail.com>
2022-06-17 11:52:33 +0900

    cafe sales Class

---
+++ Sales.py
@@ -0,0 +1,19 @@
+from Order import Order
+order=Order()
+class Sales():
+    def __init__(self):
+        self.mobile=[]
+        self.total= []
+        self.created=[]
+    def add(self, mobile, price):
+        self.mobile.append(mobile)
+        self.total.append(price)
+
+    def sum(self):
+        #매출목록 보여주고
+        print('매출목록')
+        for i in range(len(self.mobile)):
+            print(self.mobile[i], self.total[i],'원')
+        #매출총액계산/출력
+        print('매출 총액')
+        print(sum(self.total),'원')
