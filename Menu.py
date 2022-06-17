9944457d71b39baf03066bcc5e3863cc66e4da3d
choongsil <s.c.sil35@gmail.com>
2022-06-17 11:50:49 +0900

    cafe menu Class

--- Menu.py
+++ Menu.py
@@ -15,34 +15,39 @@ class Menu:
 
     def display(self):
         for i in range(len(self.name)):
-            print(self.name[i], ",", self.price[i])
+            print(i+1, ".", self.name[i], ",", self.price[i])
 
     def addnew(self, name, price):
         self.name.append(name)
         self.price.append(str(price))
+        self.save()
 
     def update(self, ndx, name, price):
         self.name[ndx]=name
         self.price[ndx]=str(price)
+        self.save()
 
     def delete(self, ndx):
         del self.name[ndx]
         del self.price[ndx]
+        self.save()
 
     def save(self):
         #self.name , self.price  의 값을 menu.txt에 쓰기하고 close
         f = open("d:/temp/menu.txt", mode='w', encoding='utf-8')
         for i in range(len(self.name)):
-            f.writelines([str(self.name[i]), ',', str(self.price[i]),'\n'])
+            f.writelines([str(self.name[i]), ',', str(self.price[i]), '\n'])
         f.close()
 
-menu=Menu()
-# menu.addnew('모카',3000)
+# menu=Menu()
+# menu.addnew('라떼',2500)
 # print(menu.name)
 # print(menu.price)
-# menu.update(0, '라떼', 5000)
-menu.delete(1)
-menu.save()
-menu.display()
-print("----------")
+# menu.update(0, '토마토주스', 5000)
+# menu.display()
+# print("----------")
+# menu.delete(3)
+# menu.save()
+# menu.display()
+# print("----------")
 
